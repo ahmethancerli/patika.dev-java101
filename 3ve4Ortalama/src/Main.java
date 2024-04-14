1ad0c11495641f09df006a05dce4fb8c109632b8
ahmet hancerli <ahmethancerli1@gmail.com>
2024-04-15 01:20:07 +0300

    3ve4Ortalama

---
+++ 3ve4Ortalama/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in) ;
+		
+		System.out.print("Bir Sayı Girin : ");
+		int sayi = input.nextInt();
+		
+		int toplam = 0;
+		int adet = 0;
+		
+		for(int i = 0; i <= sayi; i++) {
+			if(i % 3 == 0 && i % 4 == 0) {
+				toplam += i;
+				adet++;
+			}
+		}
+		
+		 double ortalama = 0;
+	        if (adet > 0) {
+	            ortalama = (double) toplam / adet;
+	        }
+	        
+	        System.out.println("0'dan " + sayi + "'e kadar olan 3 ve 4'e tam bölünen sayıların ortalaması: " + ortalama);
+	}
+}
